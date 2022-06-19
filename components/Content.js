c73b1d5b865d2a167504ab88be1318db210ed561
Burkay Sevilmiş <burkay@Burkay-MacBook-Pro.local>
2022-06-19 13:47:14 +0300

    first

---
+++ components/Content.js
@@ -0,0 +1,45 @@
+function Content() {
+  return (
+    <div class="container mx-auto">
+    <div class="grid sm:grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    <div
+      class="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100"
+    >
+      1
+    </div>
+    </div>
+</div>
+  )
+}
+
+export default Content
---
+++ utils/requests.js
@@ -0,0 +1,36 @@
+const API_KEY=process.env.API_KEY;
+
+export default{
+  fetchTrending:{
+    title:'Trending',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  fetchTopRated:{
+    title:'Top Rated',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  fetchActionMovies:{
+    title:'Action',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  fetchComedyMovies:{
+    title:'Comedy',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  fetchHorrorMovies:{
+    title:'Horror',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  Romance:{
+    title:'Romance',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  Mystery:{
+    title:'Mystery',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  Animation:{
+    title:'Animation',
+    url:`/trending/all/week?api_key=${API_KEY}&language=en-US`
+  }
+}
